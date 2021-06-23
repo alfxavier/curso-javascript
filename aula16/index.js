c75410ccaa74ef662b901b0a7aeaf33467b8f4c2
Andre Xavier <alfxavier@gmail.com>
2021-06-23 17:07:41 -0300

    aula 16 Arrays basico

---
+++ aula16/index.js
@@ -0,0 +1,30 @@
+//array, objetos e funções...
+
+const alunos = ['André', 'Luis', 'Joao', 'Joaqim', 'Anne', 'Joana'];
+
+
+console.log(typeof alunos); //retorna um objeto
+console.log(alunos instanceof Array);//Para identificar se trata-se de um Array. se true é array se false não é array
+
+
+// console.log(alunos.slice(0, -2))// faz o fatiamento igual o da string
+// delete alunos[1]; // remove o elemento mas mantém o índice.
+// console.log(alunos);
+// alunos.shift(); // removo o primeiro elemento
+// const removido1 = alunos.shift(); // remove o primeiro do array e coloca numa constante ou variável
+// alunos.pop(); // removo o ultimo elemento do Array
+// const removido2 = alunos.pop(); // remove o último do Array e coloca numa constante ou variável
+
+// alunos.unshift('Luiza') // Adiciona no ínicio do Array
+// alunos.unshift('Antonio') // Adiciona no ínicio do Array
+
+
+// alunos.push('Mária')// >> acrescente elemento no final do Array
+//alunos.length //>>mostra quantidades de elementos de array
+
+// alunos[alunos.length] = 'Luiza';//adiciona no final do Array
+// alunos[alunos.length] = 'Igos';
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Luiza';
+
+console.log(alunos);
