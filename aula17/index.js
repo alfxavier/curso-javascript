50406cea3f1ce023f66ded48f86897acdde540b0
Andre Xavier <alfxavier@gmail.com>
2021-06-23 17:45:08 -0300

    aula 17 funçoes basico

---
+++ aula17/index.js
@@ -0,0 +1,22 @@
+function saudacao (nome) {
+    console.log(`Bom dia ${nome}!`);
+    return `Bom dia ${nome}!`;
+}
+
+function soma (x = 1, y = 1){
+    const resultado = x + y;
+    return resultado;
+}
+
+// const raiz = n => n ** 0.5; //airfuction função simplificada
+
+const raiz = function(n){
+    return n ** 0.5;
+}
+console.log(raiz(9));
+
+console.log(soma());
+
+// const variavel = saudacao("André");
+
+// console.log(variavel);
