bca57a2ccdae281a8098dbabb7939ce2c956fe56
Andre Xavier <alfxavier@gmail.com>
2021-06-23 15:47:29 -0300

    aula 5 operaçoes matematicas e numeros aleatorios."
    clear
    q

---
+++ aula15/index.js
@@ -0,0 +1,19 @@
+let num1 = 9.23456;
+// let num2 = Math.floor(num1); //arredonda para baixo
+// let num2 = Math.ceil(num1); //arredonda para cima
+// let num2 = Math.round(num1); //arredonda para o número mais próximo
+// console.log(Math.max(1,2,3,34,55,66,234,4,5,-12,-44, 32));//retorna o maior número numa sequencia
+// console.log(Math.min(1,2,3,34,55,66,234,4,5,-12,-44, 32));//retorna o menor número de uma sequencia
+// console.log(Math.random());
+
+const aleatorio = Math.round(Math.random() * (60 - 0) + 0); //gera número aleatório entre 0 e 60.
+console.log(aleatorio);
+
+//raiz quadrada de um número 
+let num3 = 25;
+console.log(num3 ** 0.5);
+
+
+
+
+
