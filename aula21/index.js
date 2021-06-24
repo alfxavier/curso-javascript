35aa69ef8ac62d485661a039f40a01ccc6ffd1e2
Andre Xavier <alfxavier@gmail.com>
2021-06-24 16:27:39 -0300

    Operadores de comparaçao

---
+++ aula21/index.js
@@ -0,0 +1,12 @@
+/*Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor ou igual a
+== igualdade (valor)
+=== igualdade estrita
+!= diferente
+!== diferente estrito
+*/
+
+console.log(10>5);
