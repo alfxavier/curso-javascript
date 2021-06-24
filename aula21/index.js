1d3aeb53a9dbe2e2e53c4b06290b3c4274569245
Andre Xavier <alfxavier@gmail.com>
2021-06-24 16:47:40 -0300

    operadores logicos

--- aula21/index.js
+++ aula21/index.js
@@ -3,10 +3,10 @@
 >= maior que ou igual a
 < menor que
 <= menor ou igual a
-== igualdade (valor)
-=== igualdade estrita
-!= diferente
-!== diferente estrito
+== igualdade (valor) !! cuidado comparando string com número vai retornar TRUE !!
+=== igualdade estrita !! checa tanto o valor quanto o tipo!!
+!= diferente !! checa só o valor
+!== diferente estrito !! checa o valor e o tipo! 
 */
 
 console.log(10>5);
---
+++ aula22/index.js
@@ -0,0 +1,8 @@
+/* 
+Operadores lógicos
+&& -> AND -> E
+|| -> OR -> ou
+! -> NOT -> Não
+*/
+
+console.log();
