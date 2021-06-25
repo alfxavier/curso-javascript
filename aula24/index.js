61400d13e4ded000d2504f03c598c975f872c825
Andre Xavier <alfxavier@gmail.com>
2021-06-25 15:21:32 -0300

    aulas 24 e 24 - if else

---
+++ aula24/index.js
@@ -0,0 +1,30 @@
+/* 
+Entre 0h e 11h = bom dia; 
+entre 12h e 17 = boa tarde; 
+entre 18h e 23h = boa noite
+*/
+
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes
+// eu posso ter varios else if...
+
+
+// const hora = 10;
+
+// if (hora >= 0 && hora <= 11){
+//     console.log('Bom dia');
+// } else if (hora >= 12 && hora <= 17) {
+//     console.log('Bom tarde');
+// } else if (hora >= 18 && hora <= 23) {
+//     console.log('Bom noite');
+// } else {
+//     console.log('Olá');
+// }
+
+const tenhoGrana = true;
+
+if (tenhoGrana) {
+    console.log("vou sair de casa")
+} else {
+    console.log('Não vou sair de casa')
+}
---
+++ aula25/index.js
@@ -0,0 +1,8 @@
+//sequencia de execução de script. o if else segue uma execução de cima para baixo da esquerda para direita.
+
+const numero = 10;
+
+
+if (numero <= 10) {
+    console.log ('O número é menor ou igual a 10')
+}
