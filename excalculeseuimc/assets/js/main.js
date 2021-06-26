b6321d4fdabb5549be05b5082706d8d014e73937
Andre Xavier <alfxavier@gmail.com>
2021-06-25 22:25:45 -0300

    fim exercicio IMC

--- excalculeseuimc/assets/js/main.js
+++ excalculeseuimc/assets/js/main.js
@@ -1,25 +1,38 @@
 //não criar variável global. o certo é criar uma função.
 
-function meuEscopo () {
+function meuEscopo() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
 
-    
-    function recebeEventoForm (evento){
-        evento.preventDefault();
-        const peso = form.querySelector ('.peso');
-        const altura = form.querySelector ('.altura');
 
-        //const imc = peso / (altura*altura);
-                       
-        // if (peso > 18.5){
-        //     resultado.innerHTML = `<p>${imc}</p>`;
-        //     console.log(imc);
-        // }else{
-        //     console.log(imc);
-        // };
-        console.log(peso, altura)
-    }  
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        let imc = peso.value / (altura.value * altura.value);
+
+        imc = imc.toFixed(2);
+
+        if (imc > 1 && imc <= 18.8) {
+            resultado.innerHTML = `<p style="color:red">IMC: ${imc}. Abaixo do peso.</p>`;
+        } else if (imc > 18.5 && imc <= 24.9) {
+            resultado.innerHTML = `<p style="color:green">IMC: ${imc}. Peso normal.</p>`;
+        } else if (imc > 24.9 && imc <= 29.9) {
+            resultado.innerHTML = `<p style="color:red">IMC: ${imc}. Sobrepeso.</p>`;
+        } else if (imc > 29.9 && imc <= 34.9) {
+            resultado.innerHTML = `<p style="color:red">IMC: ${imc}. Obesidade grau 1.</p>`;
+        } else if (imc > 34.9 && imc <= 39.9) {
+            resultado.innerHTML = `<p style="color:red">IMC: ${imc}. Obesidade grau 2.</p>`;
+        } else if (imc > 39.9 && imc <= 100) {
+            resultado.innerHTML = `<p style="color:red">IMC: ${imc}. Obesidade grau 3.</p>`;
+        } else {
+            resultado.innerHTML = `<p style="color:red">Valores invalidos.</p>`;
+        } 
+        // console.log(peso.value)
+        // console.log(altura.value)
+
+    }
     form.addEventListener('submit', recebeEventoForm);
 };
 meuEscopo();
