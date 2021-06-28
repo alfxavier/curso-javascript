56545b2f24c369855435776fa3684166e3cdb0c8
Andre Xavier <alfxavier@gmail.com>
2021-06-28 17:10:35 -0300

    aula28 datas

---
+++ aula28/index.js
@@ -0,0 +1,29 @@
+// const data = new Date();
+
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth() + 1); //mes começa do 0
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Min', data.getMinutes());
+// console.log('Segundo', data.getSeconds());// 0 - Domingo e 6 -Sábado
+// console.log('Dia da Semana', data.getDay());
+// console.log(data.toString());
+// console.log(Date.now());
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`; 
+}
+
+const data = new Date();
+const dataFormatoBrasil = formataData(data);
+console.log(dataFormatoBrasil);
